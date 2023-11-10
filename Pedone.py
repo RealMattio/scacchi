e14a796980f4da408016c0259b1a69d0921259f9
Mattia Muraro <mattiamuraro8@gmail.com>
2023-11-10 14:30:51 +0100

    è stato aggiunto il file pedone ed è iniziata la sua creazione

---
+++ Pedone.py
@@ -0,0 +1,22 @@
+from Pezzo import Pezzo
+from Alfiere import Alfiere
+from Cavallo import Cavallo
+from Regina import Regina
+from Torre import Torre
+
+class Pedone(Pezzo):
+
+    def __init__(self, colore, posizione=None):
+        super().__init__(colore, posizione, 'Cavallo')
+        self.graphic_rep = '\u2659' if self.colore == 'W' else '\u265F'
+
+
+    #il pedone si leva e mette una nuova pedina!
+
+    def possibili_posizioni(self):
+        poss=[]
+        if(self.posizione[0]=='A'):
+
+        elif(self.posizione[0]=='H'):
+
+        else:
